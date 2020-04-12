88cb5b26cae58bd87cf63979099f006f24625378
LBs_MacOS <lbs_macos@LBs-MacOSs-MacBook-Air.local>
2020-04-12 22:43:01 +0200

    list4:ex3_cpp_h

---
+++ CPP2020/Lista4/zadanie_3_cpp/zadanie3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "random.h"
+
+
+int main(){
+Random objekt;
+std::cout << objekt() << std::endl;
+std::cout << objekt() << std::endl;
+std::cout << objekt() << std::endl;
+std::cout << objekt() << std::endl;
+std::cout << objekt() << std::endl;
+std::cout << objekt() << std::endl;
+
+
+return 0;
+}
