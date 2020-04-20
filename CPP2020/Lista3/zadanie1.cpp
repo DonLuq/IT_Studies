6bdfda05b3563ba265fe304e8a3f849ee2b30096
LBs_MacOS <lbs_macos@LBs-MacOSs-MacBook-Air.local>
2020-04-21 01:43:06 +0200

    1-fixed

--- CPP2020/Lista3/zadanie1.cpp
+++ CPP2020/Lista3/zadanie1.cpp
@@ -4,16 +4,14 @@
 void kopiuj(char* dokad,const char* skad){
     while(*dokad++ = *skad++)
         continue;
-    //najwyzszy priorytet ma operator wyluskiwania * dzialający na wskazniki dokad i skad,
-    //następnie dziala operator przypisania,
-    //nastepnie dziala operator ++, przemieszczajac wskaznik o jeden w pamieci.
+    //najwyzszy priorytet ma operator ikrementacji ++, a najnizszy operator przypisania =.
 
-    //operator ++ zwraca wskaznik wskazujacy na o jedna dalej wartosc pamieci niz wczesniej.
-    //operator * zwraca nam miejsce w pamieci dla objektu.
+    //operator ++ zwraca wskaznik komorki pamieci, wskazujacy na o jedna dalej wartosc adresu niz wczesniej.
+    //operator * zwraca nam miejsce w pamieci dla objektu, jest to operator wyluskiwania.
     //operator przypisania zwraca nowe przypisanie wskaznika dokad na skad
     
-    //wyrazenie w petli while, jest interpretowane jako true dopuki jest realizowane, 
-    //jezeli nie ma mozliwosci jego zrealizowania while przyjmuje false
+    //while bedzie wykonywany dopoki wartosc wyrazenia logicznego bedzie rozna od 0.
+    
 }
 
 int main(){
