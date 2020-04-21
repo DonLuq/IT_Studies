6ebac4d7885c3f7d973b836ec08fdb4bccb3efef
LBs_MacOS <lbs_macos@LBs-MacOSs-MacBook-Air.local>
2020-04-22 00:33:20 +0200

    a little fix

--- CPP2020/Lista2/zadanie1.cpp
+++ CPP2020/Lista2/zadanie1.cpp
@@ -4,24 +4,26 @@
 
 using namespace std;
 
-int smalest(vector<int> & N){
+int smallest(vector<int> & N){
     if(N.size()==0){ 
        return INT_MIN;
     }
     int number = N[0];
-    for(int i=0; i<N.size();i++){
+    for(int i=1; i<N.size();i++){
         if(N[i] <= number){
             number = N[i];
         }
     }
     return number;
+    //petla wykonywana od i=1 poniewaz pierwsze przejscie dla petli zawsze bedzie spelnione bo porownanie N[0]==N[i=0]
 }
 
 int main(){
 
     vector<int>  wektor{31,321,32,43,12,54,-10,1};
     vector<int>  vek{};
-    int smallest_number =  smalest(wektor);
+    vector<int>  vekt{1,2};
+    int smallest_number =  smallest(wektor);
     cout<<"Najmniesza wartosc wektora to: "<<smallest_number<<endl;
 
     return 0;
