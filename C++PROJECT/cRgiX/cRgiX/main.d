7ebb665b119d901a02ab520651b396d357d7e0b3
DonLuq <luqas828@gmail.com>
2020-11-25 21:22:24 +0100

    sprzatanko

--- C++PROJECT/cRgiX/cRgiX/main.d
+++
@@ -1,4 +0,0 @@
-Objects/main.o: \
- /home/donluq/GIT/IT_Studies/C++PROJECT/cRgiX/cRgiX//main.cpp \
- /home/donluq/GIT/IT_Studies/C++PROJECT/cRgiX/cRgiX//player.hpp \
- /home/donluq/GIT/IT_Studies/C++PROJECT/cRgiX/cRgiX//window.hpp
--- C++PROJECT/cRgiX/cRgiX/window.d
+++
@@ -1,3 +0,0 @@
-Objects/window.o: \
- /home/donluq/GIT/IT_Studies/C++PROJECT/cRgiX/cRgiX//window.cpp \
- /home/donluq/GIT/IT_Studies/C++PROJECT/cRgiX/cRgiX//window.hpp
